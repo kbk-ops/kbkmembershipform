84c5d70bfb691f323a738dabb37cca728de873de
kbk-ops <yeesphilippines@gmail.com>
2026-09-09 23:37:48 +0800

    Delete reg directory

--- reg/form/auth.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const referrerID = sessionStorage.getItem('referrerID');
-    const registerEmail = sessionStorage.getItem('registerEmail');
-
-    if (!referrerID || !registerEmail) {
-        console.warn("Session data missing. Redirecting to home...");
-        window.location.href = "../index.html";
-    }
-})();
